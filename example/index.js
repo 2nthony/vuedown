8841667d1ad001caf616bf313fa7cdf807dc2514
evillt <ijoec123@gmail.com>
2019-05-25 14:14:12 +0800

    chore: drop files

--- example/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import Index from './Index.md'
-
-/* eslint-disable no-new */
-new Vue({
-  render: h => h(Index)
-}).$mount('#app')
--- types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Options } from 'markdown-it'
-
-declare function vuedown(source: string, opts: Options): string
-
-export = vuedown
-export as namespace vuedown
