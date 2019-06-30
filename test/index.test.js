d39719b1c066dfffd63192a1e129e368cf5b7e5f
evillt <ijoec123@gmail.com>
2019-06-30 17:01:32 +0800

    test: add test snapshot

---
+++ test/index.test.js
@@ -0,0 +1,44 @@
+import test from 'ava'
+import vuedown from '../'
+
+function snapshot(title, text, options) {
+  test(title, t => {
+    t.snapshot(vuedown(text, options))
+  })
+}
+
+snapshot(
+  'basic',
+  `
+# Say hi
+
+<greet name="evillt" />
+`
+)
+
+snapshot(
+  'with vue features',
+  `
+# I'm {{ name }}
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'evillt'
+    }
+  }
+}
+</script>
+`
+)
+
+snapshot(
+  'wrap html',
+  `
+# Hi there
+`,
+  {
+    wrapHTML: html => `<div class="wrapper">${html}</div>`
+  }
+)
